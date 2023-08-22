daece1638bc782a884add3ad596797fa01f5bea3
Rachel Manlove <111536418+rachelmanlove-hpg@users.noreply.github.com>
2023-08-22 09:35:34 -0500

    potions making working

--- src/components/village/SchoolPotionsMaking.vue
+++ src/components/village/SchoolPotionsMaking.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center">
-        <div class="grid grid-cols-1">
+        <div v-if="potionCrafting" class="grid grid-cols-1">
             <div class="mt-10 flex justify-center"> 
                 <img 
                     src="../../assets/images/village_apothecary/Apothecary_Shopkeeper.png"
@@ -22,21 +22,42 @@
                 <p class="my-3">You're going to need:</p>
             </div>
             <div class="pt-3 bg-white bg-opacity-70 rounded-b-3xl"> 
-                <div class="flex justify-center items-center text-center">
+                <div class="flex justify-center items-center text-center pb-3">
                     <div class="mx-2 grid grid-cols-1">
                         <div class="flex justify-center"> 
                             <img :src="renderedPotion.needImg" class="h-20"/>
                         </div>
                         
                         <p>{{ renderedPotion.need }}</p>
-                        <p>x {{ renderedPotion.qty }}</p>
+                        <p>{{ renderedPotion.qty }}</p>
                     </div>
                     <div class="mx-2 grid grid-cols-1">
                         <div class="flex justify-center"> 
                             <img :src="renderedPotion.needImg2" class="h-20"/>
                         </div>                        
                         <p>{{ renderedPotion.need2 }}</p>
-                        <p>x {{ renderedPotion.qty2 }}</p>
+                        <p>{{ renderedPotion.qty2 }}</p>
+                    </div>
+                    <div class="mx-2 grid grid-cols-1">
+                        <div class="flex justify-center"> 
+                            <img :src="renderedPotion.needImg3" class="h-20"/>
+                        </div>                        
+                        <p>{{ renderedPotion.need3 }}</p>
+                        <p>{{ renderedPotion.qty3 }}</p>
+                    </div>
+                    <div class="mx-2 grid grid-cols-1">
+                        <div class="flex justify-center"> 
+                            <img :src="renderedPotion.needImg4" class="h-20"/>
+                        </div>                        
+                        <p>{{ renderedPotion.need4 }}</p>
+                        <p>{{ renderedPotion.qty4 }}</p>
+                    </div>
+                    <div class="mx-2 grid grid-cols-1">
+                        <div class="flex justify-center"> 
+                            <img :src="renderedPotion.needImg5" class="h-20"/>
+                        </div>                        
+                        <p>{{ renderedPotion.need5 }}</p>
+                        <p>{{ renderedPotion.qty5 }}</p>
                     </div>
                 </div>
             </div>
@@ -53,12 +74,36 @@
                     </li>
                 </ul>
             </div> 
-
             <div class="flex justify-center"> 
                 <button @click="emitComeBackLater" class="mt-10 px-2 py-2 w-32 border border-gray-500 rounded-xl bg-stone-400 hover:bg-stone-600 hover:text-white text-center">
                     Come Back Later</button>
             </div>  
-        </div>  
+        </div> 
+        
+        <div v-if="potionReady" class="grid grid-cols-1"> 
+            <div class="mt-10 flex justify-center"> 
+                <img 
+                    src="../../assets/images/village_apothecary/Apothecary_Shopkeeper.png"
+                    class="h-72 border border-gray-600"
+                />
+            </div>
+            <div class="text-center pt-5 bg-white bg-opacity-70 rounded-t-3xl">  
+                <h1 class="text-xl font-semibold text-gray-700">Looks like your</h1>
+                <h2 class="text-2xl font-bold text-slate-700 px-4">{{ renderedPotion.id2 }}?</h2>
+                <p class="text-xl font-semibold text-gray-700">is ready!</p>
+            </div>
+            <div class="bg-white bg-opacity-70 flex justify-center pt-5"> 
+                <img 
+                    src="../../assets/images/village_school/school_full_caldron.png" 
+                    class="h-52 border border-gray-300 rounded-2xl"
+                />
+            </div>
+            <div class="flex justify-center bg-white bg-opacity-70 pb-5 rounded-b-3xl"> 
+                <button @click="checkInventory" class="mt-5 px-2 py-2 w-32 border border-gray-500 rounded-xl bg-[#a6bf8e] hover:bg-green-100 text-center">
+                    Collect potion</button>
+            </div>  
+        </div>
+        
     </div>
 </template>
 
@@ -84,6 +129,9 @@
     function emitComeBackLater() {
         emit('emitComeBackLater');
     }
+
+    const potionCrafting = ref(true);
+    const potionReady = ref(false);
 
     const chosenItemId = ref('');    
     const chosenItem = computed(function() {
@@ -131,37 +179,7 @@
                 renderedPotion.value.qty5 = (renderedPotion.value.qty5 - 1);
                 playerStore.playerPacked.splice(x, 1);
             }
-        } else if (chosenItem.value.name === renderedPotion.value.need6) {
-            if (renderedPotion.value.qty6 - 1 < 0 ) {
-                renderedPotion.value.qty6 = 0;
-            } else {
-                renderedPotion.value.qty6 = (renderedPotion.value.qty6 - 1);
-                playerStore.playerPacked.splice(x, 1);
-            }
-        } else if (chosenItem.value.name === renderedPotion.value.need7) {
-            if (renderedPotion.value.qty7 - 1 < 0 ) {
-                renderedPotion.value.qty7 = 0;
-            } else {
-                renderedPotion.value.qty7 = (renderedPotion.value.qty7 - 1);
-                playerStore.playerPacked.splice(x, 1);
-            }
-        } else if (chosenItem.value.name === renderedPotion.value.need8) {
-            if (renderedPotion.value.qty8 - 1 < 0 ) {
-                renderedPotion.value.qty8 = 0;
-            } else {
-                renderedPotion.value.qty8 = (renderedPotion.value.qty8 - 1);
-                playerStore.playerPacked.splice(x, 1);
-            }
-        } else if (chosenItem.value.name === renderedPotion.value.need9) {
-            if (renderedPotion.value.qty9 - 1 < 0 ) {
-                renderedPotion.value.qty9 = 0;
-            } else {
-                renderedPotion.value.qty8 = (renderedPotion.value.qty9 - 1);
-                playerStore.playerPacked.splice(x, 1);
-            }
         }
-        
-        const potionReady = ref(false);
 
         const q2 = ref(0)
         if (!(renderedPotion.value.qty2 > 0)) {q2.value = 0} else (q2.value = renderedPotion.value.qty2)
@@ -171,28 +189,29 @@
         if (!(renderedPotion.value.qty4 > 0)) { q4.value = 0} else (q4.value = renderedPotion.value.qty4)
         const q5 = ref(0)
         if (!(renderedPotion.value.qty5 > 0 )) { q5.value = 0} else (q5.value = renderedPotion.value.qty5)
-        const q6 = ref(0)
-        if (!(renderedPotion.value.qty6 > 0)) { q6.value = 0} else (q6.value = renderedPotion.value.qty6)
-        const q7 = ref(0)
-        if (!(renderedPotion.value.qty7 > 0)) { q7.value = 0} else (q7.value = renderedPotion.value.qty7)
-        const q8 = ref(0)
-        if (!(renderedPotion.value.qty8 > 0)) { q8.value = 0} else (q8.value = renderedPotion.value.qty8)
-        const q9 = ref(0)
-        if (!(renderedPotion.value.qty9 > 0)) { q9.value = 0} else (q9.value = renderedPotion.value.qty9)
-        console.log("q2: " +q2.value)
 
-        if ((renderedPotion.value.qty + q2.value + q3.value + q4.value + q5.value + q6.value + q7.value + q8.value + q9.value) - 1 < 0) {
+        if ((renderedPotion.value.qty + q2.value + q3.value + q4.value + q5.value) - 1 < 0) {
+            potionCrafting.value = false;
             potionReady.value = true;
-            // renderedPotion.value.complete = true;  // do i need this? YES - for unlocking features in response to quest completion
-            // renderedPotion.value.active = false;   // do i need this? YES - for quest complete button on main page
-            // questDone.value = true;
-            // playerStore.playerXP = playerStore.playerXP + renderedPotion.value.playerXP;
-            // playerStore.playerTotalXP = playerStore.playerTotalXP + renderedPotion.value.playerXP;
-            // playerStore.coinOnHand = playerStore.coinOnHand + renderedPotion.value.coins;
-            // let y = playerStore.playerActiveQuests.findIndex(quest => quest.id === renderedPotion.value.id);
-            // playerStore.playerActiveQuests.splice(y,1);
-            // playerStore.playerCompletedQuests.push(renderedPotion.value);
         }
+    }
+
+    const insufficientSpace = ref(false);
+    function checkInventory() {
+        if (playerStore.playerPacked.length >= playerStore.carryCapacity) {
+        insufficientSpace.value = true;
+      } else (collectItem())
+    }
+    function collectItem() {
+        playerStore.playerPacked.push(renderedPotion.value);
+        renderedPotion.value.qty = renderedPotion.value.baseQty;
+        renderedPotion.value.qty2 = renderedPotion.value.baseQty2;
+        renderedPotion.value.qty3 = renderedPotion.value.baseQty3;
+        renderedPotion.value.qty4 = renderedPotion.value.baseQty4;
+        renderedPotion.value.qty5 = renderedPotion.value.baseQty5;
+        potionCrafting.value = true;
+        potionReady.value = false;
+        // emitComeBackLater();
     }
 
 </script>
